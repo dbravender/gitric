25e5070a575de1ae7e20d6ede71297ab424cea87
Dan Bravender <dan.bravender@gmail.com>
2016-08-21 16:57:08 -0400

    Add a route to send parrot images

--- bluegreen-example/app.py
+++ bluegreen-example/app.py
@@ -1,9 +1,14 @@
 import os
 
-from flask import Flask
+from flask import Flask, send_from_directory
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
     return "Hello 0-downtime %s World!" % os.environ.get('BLUEGREEN', 'bland')
+
+
+@app.route("/parrots/<path:path>")
+def parrot(path):
+    return send_from_directory(os.path.join('parrots', 'parrots'), path)
